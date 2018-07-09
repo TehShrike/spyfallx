cc9deea0fb67bca2e113d05e1df266ee2bc198d2
Josh Duff <me@JoshDuff.com>
2018-07-09 11:00:50 -0500

    ES modules in the browser!

---
+++ public/futch.js
@@ -0,0 +1,24 @@
+export const futch = async(url, options = {}) => {
+	const headers = new Headers()
+	headers.append(`content-type`, `application/json`)
+
+	if (options.headers) {
+		Object.keys(options.headers).forEach(header => {
+			headers.append(header, options.headers[header])
+		})
+	}
+
+	const response = await fetch(url, {
+		method: options.method || `GET`,
+		body: options.body && JSON.stringify(options.body),
+		headers,
+	})
+
+	if (response.status < 200 || response.status >= 300) {
+		throw new Error(`Server returned status ${ response.status }`)
+	}
+
+	return response.json()
+}
+
+export const post = (url, body = {}) => futch(url, { method: `POST`, body })
